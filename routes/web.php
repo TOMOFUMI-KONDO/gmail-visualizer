86289ee25d366018ee123fc6532c6c02e5535579
sofuetakuma112 <kawahagi0620@gmail.com>
2020-09-12 09:31:45 +0900

    react setUp

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/{any}', function(){
+    return view('App');
+})->where('any', '.*');
