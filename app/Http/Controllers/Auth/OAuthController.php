9cf2a8126055f3b05f50f575971281cd6df28a1d
sofuetakuma112 <kawahagi0620@gmail.com>
2020-09-15 02:40:43 +0900

    Google認証を追加

---
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
+use Socialite;
+
+class OAuthController extends Controller
+{
+    /**
+     * 各SNSのOAuth認証画面にリダイレクトして認証
+     * @param string $provider サービス名
+     * @return mixed
+     */
+    public function socialOAuth(string $provider)
+    {
+        // google認証ページにリダイレクト
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * 各サイトからのコールバック
+     * @param string $provider サービス名
+     * @return mixed
+     */
+    public function handleProviderCallback($provider)
+    {
+        // socialUserにGoogleから渡されたユーザー情報が入っている
+        $socialUser = Socialite::driver($provider)->user();
+        // 実行時に、指定したパラメータを持ったレコードが無ければ、新しくインスタンスを作成します。
+        $user = User::firstOrNew(['email' => $socialUser->getEmail()]);
+
+        // すでに会員になっている場合の処理を書く
+        // そのままログインさせてもいいかもしれない
+        if ($user->exists) {
+            abort(403);
+        }
+
+        $user->name = $socialUser->getNickname();
+        $user->provider_id = $socialUser->getId();
+        $user->provider_name = $provider;
+        $user->save();
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::prefix('auth')->middleware('guest')->group(function() {
+
+    // whereメソッドでパラメーターに成約を設けている
+    Route::get('/{provider}', 'App\Http\Controllers\Auth\OAuthController@socialOAuth')
+        ->where('provider','google')
+        ->name('socialOAuth');
+ 
+     Route::get('/{provider}/callback', 'App\Http\Controllers\Auth\OAuthController@handleProviderCallback')
+         ->where('provider','google')
+         ->name('oauthCallback');
+ });
+
 //'api'以外から始まるルーティングを全てReactに投げる。
 Route::get('/{any}', function(){
     return view('app');
