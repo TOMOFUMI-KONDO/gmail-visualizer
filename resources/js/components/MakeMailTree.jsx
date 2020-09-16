4d40026dba1e73284d8faebf58496e894095e4b0
yusuke-suehiro <63089903+yusuke-suehiro@users.noreply.github.com>
2020-09-17 02:08:54 +0900

    revise conflict2

---
+++ resources/js/components/MakeMailTree.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EachMail from "./EachMail";
+
+function MakeMailTree(props) {
+    const mailtp = props.mailtp;
+    const setNowBody = props.setNowBody;
+    const tp = mailtp.map((mail) => <EachMail key={mail} mail={mail} setNowBody={setNowBody} />);
+
+    const result = tp;
+    return result;
+}
+
+export default MakeMailTree;
---
+++ resources/js/components/MakeTree.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import SplitPane from "react-split-pane";
+import Typography from "@material-ui/core/Typography";
+import MakeMailTree from "./MakeMailTree";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    content: {
+        borderLeft: "1px solid black",
+        padding: "10px 0px 100vh 10px",
+        backgroundColor: "white",
+    },
+});
+
+function MakeTree(props) {
+    const classes = useStyles();
+    const mails = props.mails;
+    const mailTrees = <MakeMailTree mailtp={mails} />;
+
+    const layout = (
+        <SplitPane className="split" split="vertical" minSize={200} defaultSize={500} maxSize={1000}>
+            {mailTrees}
+            <Typography className={classes.content} variant="h5" component="h3">
+                This is a sheet of paper.
+            </Typography>
+        </SplitPane>
+    );
+    return layout;
+}
+
+export default MakeTree;
